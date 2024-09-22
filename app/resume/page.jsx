1890d53b3c1c17d77cbc85c972470e102ced1439
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-22 19:47:29 +0530

    modified the resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,5 +1,350 @@
+"use client";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+  FaPhp,
+} from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiNextdotjs,
+  SiTypescript,
+  SiMongodb,
+  SiMysql,
+} from "react-icons/si";
+
+const about = {
+  title: "About Me",
+  description:
+    "I excel at creating intuitive, visually appealing web applications that enhance user experience. Passionate about turning creative ideas into digital realities, I focus on seamless interfaces and responsive, full-stack solutions.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Menath Nadungoda",
+    },
+    {
+      fieldName: "Mobile",
+      fieldValue: "(+94) 76 549 3072",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "nadungmenath1@gmail.com",
+    },
+    {
+      fieldName: "Location",
+      fieldValue: "Colombo, Sri Lanka",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "03+ years",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Sri Lankan",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Sinhala",
+    },
+  ],
+};
+
+const experience = {
+  icon: "/assets/resume/badge.svg",
+  title: "My Experience",
+  description:
+    "I have 3+ years of experience in web development, working with various technologies and frameworks.",
+  items: [
+    {
+      position: "Frontend Developer",
+      company: "Freelance",
+      date: "2024 - Present",
+    },
+    {
+      position: "Full Stack Developer",
+      company: "Smart Tech Solutions",
+      date: "2023 Jun - 2024",
+    },
+    {
+      position: "UI/Ux Designer",
+      company: "Freelance",
+      date: "2022 Sep - 2023 Jun",
+    },
+    {
+      position: "Trainee Intern",
+      company: "People's Bank",
+      date: "2022 Feb - 2022 Aug",
+    },
+    {
+      position: "Assistant Intern",
+      company: "Bank of Ceylon",
+      date: "2021 Jul - 2022 Mar",
+    },
+    {
+      position: "E-commerce Startup",
+      company: "Freelance Web Developer",
+      date: "2021 - 2022 Sep",
+    },
+    {
+      position: "UI/Ux Designer",
+      company: "Design Studio",
+      date: "2020 - 2021",
+    },
+    {
+      position: "Junior Video Editor",
+      company: "ModernViz Inc.",
+      date: "2020 Feb - 2020",
+    },
+    {
+      position: "Documentary Filmmaker",
+      company: "AloneMax Station",
+      date: "2019 - 2020 Feb",
+    },
+    {
+      position: "Content Creator",
+      company: "Freelance",
+      date: "2018 - 2019",
+    },
+  ],
+};
+
+const education = {
+  icon: "/assets/resume/cap.svg",
+  title: "My Education",
+  description:
+    "I am an undergraduate with a BSc. (Hons.) in Computing & Information Systems (CIS) from the Sabaragamuwa University of Sri Lanka.",
+  items: [
+    {
+      degree: "BSc(Hons) in Computing & Information Systems",
+      university: "Sabaragamuwa University",
+      date: "2022 - Present",
+    },
+    {
+      degree: "General Certificate of Education (G.C.E) A/L",
+      university: "St. Aloysisus College, Galle",
+      date: "2018 - 2020",
+    },
+    {
+      degree: "General Certificate of Education (G.C.E) O/L",
+      university: "All Saints College, Galle",
+      date: "2017 - 2019",
+    },
+  ],
+};
+
+const skills = {
+  title: "My Skills",
+  description:
+    "I have experience working with a variety of technologies and frameworks.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "HTML 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "CSS 3",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "Tailwind CSS",
+    },
+    {
+      icon: <FaJs />,
+      name: "JavaScript",
+    },
+    {
+      icon: <SiTypescript />,
+      name: "TypeScript",
+    },
+    {
+      icon: <FaReact />,
+      name: "React JS",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "Next JS",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "Node JS",
+    },
+    {
+      icon: <FaPhp />,
+      name: "PHP",
+    },
+    {
+      icon: <SiMongodb />,
+      name: "MongoDB",
+    },
+    {
+      icon: <SiMysql />,
+      name: "MySQL",
+    },
+    {
+      icon: <FaFigma />,
+      name: "Figma",
+    },
+  ],
+};
+
 const Resume = () => {
-  return <div>Resume</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="about"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="about">About</TabsTrigger>
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+          </TabsList>
+
+          <div className="min-h-[70vh] w-full">
+            <TabsContent
+              value="about"
+              className="w-full text-center xl:text-left"
+            >
+              <div className="flex flex-col gap-[30px]">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[750px] mx-auto xl:mx-0">
+                  {about.info.map((item, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="flex items-center justify-center gap-4 xl:justify-start"
+                      >
+                        <span className="text-white/60">{item.fieldName}</span>
+                        <span className="text-xl">{item.fieldValue}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </TabsContent>
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.date}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.position}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {education.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.date}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.degree}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.university}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {skills.description}
+                  </p>
+                </div>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                  {skills.skillList.map((item, index) => {
+                    return (
+                      <li key={index}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                              <div className="text-6xl transition-all duration-300 group-hover:text-accent">
+                                {item.icon}
+                              </div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p>{item.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
