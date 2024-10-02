d5096a41687b3d4b2a7c10d3c3fe739ce51a1925
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-03 00:46:52 +0530

    Refactor MessageDialog component and update styles

--- components/MessageDialog.jsx
+++ components/MessageDialog.jsx
@@ -10,24 +10,28 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
+import { FaPaperPlane } from "react-icons/fa";
 
 const MessageDialog = ({ isOpen, onClose }) => {
   return (
     <AlertDialog open={isOpen} onOpenChange={onClose}>
-      <AlertDialogContent className="max-w-[450px] p-6 sm:max-w-xl sm:p-8">
-        <AlertDialogHeader>
-          <AlertDialogTitle className="text-2xl text-accent sm:text-2xl">
-            Message Sent Successfully!
+      <AlertDialogContent className="max-w-[350px] p-6 rounded-lg bg-[#1e1e23] sm:max-w-[500px] sm:p-8 shadow-lg">
+        <AlertDialogHeader className="flex flex-col items-center">
+          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-accent text-primary">
+            <FaPaperPlane size={32} />
+          </div>
+          <AlertDialogTitle className="text-2xl font-semibold text-accent sm:text-3xl">
+            Message Sent!
           </AlertDialogTitle>
-          <AlertDialogDescription className="mt-2 text-sm text-white sm:text-base">
-            Thank you for reaching out! I have received your message and will
-            get back to you as soon as possible.
+          <AlertDialogDescription className="mt-2 text-sm leading-6 text-center text-white/80 sm:text-base">
+            Thank you for contacting me! I have received your message and will
+            respond as soon as possible.
           </AlertDialogDescription>
         </AlertDialogHeader>
-        <AlertDialogFooter>
+        <AlertDialogFooter className="flex justify-center mt-6">
           <AlertDialogAction
             onClick={onClose}
-            className="items-center justify-center w-full px-6 py-2 text-lg font-semibold text-primary bg-accent hover:bg-accent-hover sm:w-auto sm:py-3"
+            className="px-8 py-3 text-lg font-semibold text-white rounded-full bg-accent hover:bg-accent-hover focus:outline-none focus:ring-4 focus:ring-accent-hover focus:ring-opacity-50 sm:px-10 sm:py-3"
           >
             Close
           </AlertDialogAction>
